6af531a038a4c83ea2c58117af3cd4b8eec5e27b
Alexandr Polikarpov <nub.and.co@gmail.com>
2022-01-12 01:43:24 +0300

    Сдача дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        int a = 1;
+        while (a <= 10) {
+            System.out.print(a + " ");
+            a++;
+        }
+        System.out.print("\n");
+        for (int b = 10; b >= 1; b = b - 1) {
+            System.out.print(b + " ");
+        }
+
+        System.out.print("\n");
+        int friday = 4;
+        for ( ; friday <= 31; friday = friday + 7) {
+            System.out.println("Сегодня пятница " + friday + "-е число. Необходимо подготовить отчёт.");
+        }
+
+
+        int cometYear = 0;
+        int startYear = 1822;
+        int finishYear = 2122;
+        for ( ; cometYear <= finishYear; cometYear = cometYear + 79) {
+            if (cometYear >= startYear) {
+            System.out.println(cometYear);}
+
+        }
     }
 }
